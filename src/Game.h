a80e9d9f29ceed911bf85858602e8009c55e1df0
massanaRoger <94066091+massanaRoger@users.noreply.github.com>
2024-06-06 16:26:32 +0200

    Game class done

---
+++ src/Game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H 
+
+// Represents the current state of the game
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+class Game {
+	public:
+		GameState State;
+		bool Keys[1024];
+		unsigned int Width, Height;
+		Game(unsigned int width, unsigned int height);
+		~Game();
+		void Init();
+		void ProcessInput(float dt);
+		void Update(float dt);
+		void Render();
+};
+
+#endif
